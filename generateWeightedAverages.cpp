eb5c2730846f038010647435e7b0d3105f803c60
Brian Vernarsky <soulish@gmail.com>
2015-12-09 02:00:45 -0500

    Replace BOOST_FOREACH with for loops

--- generateWeightedAverages.cpp
+++ generateWeightedAverages.cpp
@@ -8,8 +8,6 @@
 #include <boost/algorithm/string.hpp>
 #include "src/ConstantTeam5YearAverages.h"
 #include "src/ConstantTeamNeutralRatios.h"
-#include "src/ConstantTeamPointDifferentials.h"
-#include "src/ConstantTeamWinRatios.h"
 #include "src/Team.h"
 #include "src/TeamGame.h"
 #include "src/Pcts.h"
@@ -60,74 +58,74 @@ int main(int argc,char *argv[]) {
     Team *a;
     TeamGame *tg;
 
-    BOOST_FOREACH(teamHash::value_type &team, teams) {
-                    std::cout << team.first << std::endl;
-                    a = team.second;
-                    vector<double> x;
-                    ofstream waveragesFile,averagesFile;
+    for (auto &team : teams) {
+        std::cout << team.first << std::endl;
+        a = team.second;
+        std::vector<double> x;
+        std::ofstream waveragesFile,averagesFile;
 
-                    sprintf(path,"%s/cpp/NCAA_C/teams/%i/teams.%s.averages.d",
-                            homePath,year,boost::replace_all_copy(a->getName()," ","_").c_str());
+        sprintf(path,"%s/cpp/NCAA_C/teams/%i/teams.%s.averages.d",
+                homePath,year,boost::replace_all_copy(a->getName()," ","_").c_str());
 
-                    if (!overWrite) {
-                        struct stat buffer;
-                        if (stat(path, &buffer) == 0) continue;
-                    }
+        if (!overWrite) {
+            struct stat buffer;
+            if (stat(path, &buffer) == 0) continue;
+        }
 
-                    averagesFile.open(path);
-                    sprintf(path,"%s/cpp/NCAA_C/teams/%i/teams.%s.waverages.d",
-                            homePath,year,boost::replace_all_copy(a->getName()," ","_").c_str());
-                    waveragesFile.open(path);
-                    boost::gregorian::day_iterator ditr(ConstantSeasonInfo::Instance()->get(year,"season start") +
-                                                        boost::gregorian::date_duration(14));//2 weeks after start of season
-                    for (; ditr <= ConstantSeasonInfo::Instance()->get(year,"tournament start"); ++ditr) {
-                        std::cout<< "Processing date: " <<
-                                boost::gregorian::to_iso_extended_string(*ditr) << "\r";//std::endl;
-                        std::fflush(stdout);
-                        x = a->calcWeightedAverage(*ditr);
+        averagesFile.open(path);
+        sprintf(path,"%s/cpp/NCAA_C/teams/%i/teams.%s.waverages.d",
+                homePath,year,boost::replace_all_copy(a->getName()," ","_").c_str());
+        waveragesFile.open(path);
+        boost::gregorian::day_iterator ditr(ConstantSeasonInfo::Instance()->get(year,"season start") +
+                                            boost::gregorian::date_duration(14));//2 weeks after start of season
+        for (; ditr <= ConstantSeasonInfo::Instance()->get(year,"tournament start"); ++ditr) {
+            std::cout<< "Processing date: " <<
+            boost::gregorian::to_iso_extended_string(*ditr) << "\r";
+            std::fflush(stdout);
+            x = a->calcWeightedAverage(*ditr);
 
-                        tg = a->GameOnDate(*ditr);
+            tg = a->GameOnDate(*ditr);
 
-                        averagesFile << a->getName();
-                        averagesFile << "," << ditr->year() << "," << ditr->month().as_number() << "," << ditr->day();
-                        averagesFile << "," << doubleFormatter(x[0], 0);
-                        for (int ii = 1; ii < 13; ii++) {
-                            if (ii % 2 == 1) averagesFile << "," << doubleFormatter(x[ii], 0);
-                            if (ii % 2 == 0) averagesFile << "," << doubleFormatter(x[ii], 3);
-                        }
-                        averagesFile << "," << doubleFormatter(x[13], 0);
-                        for (int ii = 14; ii < 26; ii++) {
-                            if (ii % 2 == 0) averagesFile << "," << doubleFormatter(x[ii], 0);
-                            if (ii % 2 == 1) averagesFile << "," << doubleFormatter(x[ii], 3);
-                        }
-                        if (tg) averagesFile << "," << doubleFormatter(tg->getSpread(), 1);
-                        else averagesFile << "," << "0.0";
-                        averagesFile << std::endl;
+            averagesFile << a->getName();
+            averagesFile << "," << ditr->year() << "," << ditr->month().as_number() << "," << ditr->day();
+            averagesFile << "," << doubleFormatter(x[0], 0);
+            for (int ii = 1; ii < 13; ii++) {
+                if (ii % 2 == 1) averagesFile << "," << doubleFormatter(x[ii], 0);
+                if (ii % 2 == 0) averagesFile << "," << doubleFormatter(x[ii], 3);
+            }
+            averagesFile << "," << doubleFormatter(x[13], 0);
+            for (int ii = 14; ii < 26; ii++) {
+                if (ii % 2 == 0) averagesFile << "," << doubleFormatter(x[ii], 0);
+                if (ii % 2 == 1) averagesFile << "," << doubleFormatter(x[ii], 3);
+            }
+            if (tg) averagesFile << "," << doubleFormatter(tg->getSpread(), 1);
+            else averagesFile << "," << "0.0";
+            averagesFile << std::endl;
 
-                        waveragesFile << a->getName();
-                        waveragesFile << "," << ditr->year() << "," << ditr->month().as_number() << "," << ditr->day();
-                        waveragesFile << "," << doubleFormatter(x[26], 0);
-                        for (int ii = 27; ii < 39; ii++) {
-                            if (ii % 2 == 1) waveragesFile << "," << doubleFormatter(x[ii], 0);
-                            if (ii % 2 == 0) waveragesFile << "," << doubleFormatter(x[ii], 3);
-                        }
-                        waveragesFile << "," << doubleFormatter(x[39], 0);
-                        for (int ii = 40; ii < 52; ii++) {
-                            if (ii % 2 == 0) waveragesFile << "," << doubleFormatter(x[ii], 0);
-                            if (ii % 2 == 1) waveragesFile << "," << doubleFormatter(x[ii], 3);
-                        }
-                        waveragesFile << "," << doubleFormatter(x[52], 3);//rpi
-                        waveragesFile << "," << doubleFormatter(x[53], 3);//srs
-                        waveragesFile << "," << doubleFormatter(x[54], 3);//sos
-                        waveragesFile << "," << doubleFormatter(x[55], 0);//num_games
-                        if (tg) waveragesFile << "," << doubleFormatter(tg->getSpread(), 1);//spread
-                        else waveragesFile << "," << "0.0";
-                        waveragesFile << std::endl;
-                    }
-                    averagesFile.close();
-                    waveragesFile.close();
-                    std::cout << std::endl;
-                }
+            waveragesFile << a->getName();
+            waveragesFile << "," << ditr->year() << "," << ditr->month().as_number() << "," << ditr->day();
+            waveragesFile << "," << doubleFormatter(x[26], 0);
+            for (int ii = 27; ii < 39; ii++) {
+                if (ii % 2 == 1) waveragesFile << "," << doubleFormatter(x[ii], 0);
+                if (ii % 2 == 0) waveragesFile << "," << doubleFormatter(x[ii], 3);
+            }
+            waveragesFile << "," << doubleFormatter(x[39], 0);
+            for (int ii = 40; ii < 52; ii++) {
+                if (ii % 2 == 0) waveragesFile << "," << doubleFormatter(x[ii], 0);
+                if (ii % 2 == 1) waveragesFile << "," << doubleFormatter(x[ii], 3);
+            }
+            waveragesFile << "," << doubleFormatter(x[52], 3);//rpi
+            waveragesFile << "," << doubleFormatter(x[53], 3);//srs
+            waveragesFile << "," << doubleFormatter(x[54], 3);//sos
+            waveragesFile << "," << doubleFormatter(x[55], 0);//num_games
+            if (tg) waveragesFile << "," << doubleFormatter(tg->getSpread(), 1);//spread
+            else waveragesFile << "," << "0.0";
+            waveragesFile << std::endl;
+        }
+        averagesFile.close();
+        waveragesFile.close();
+        std::cout << std::endl;
+    }
 
 
     return 0;
