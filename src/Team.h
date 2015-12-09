ecedb0911d1ec84783ab19192450c9f5d5ce85a1
Brian Vernarsky <soulish@gmail.com>
2015-12-09 02:06:52 -0500

    Very minor change.  Change location of function declaration.

--- src/Team.h
+++ src/Team.h
@@ -32,7 +32,6 @@ public:
     const std::unordered_map<std::string, TeamGame *> &getGamesByDate() const {  return gamesByDate;  }
     const std::unordered_map<std::string, TeamAverage *> &getAveragesByDate() const  {  return averagesByDate;  }
     const std::unordered_map<std::string, TeamWAverage *> &getWAveragesByDate() const {  return waveragesByDate;  }
-    std::vector<double> calcWeightedAverage(boost::gregorian::date date);
     TeamGame *GameOnDate(boost::gregorian::date d) const;
     TeamAverage *AverageOnDate(boost::gregorian::date d) const;
     TeamWAverage *WAverageOnDate(boost::gregorian::date d) const;
@@ -46,6 +45,8 @@ public:
     void addGames(std::string);
     void addAverages(std::string);
     void addWAverages(std::string);
+
+    std::vector<double> calcWeightedAverage(boost::gregorian::date date);
 
     //static functions
     static int getNumTeams(){  return (int)teams.size();  }
