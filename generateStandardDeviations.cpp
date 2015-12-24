e962dd56637dc4159e5f89378f65b1d38362edd7
Brian Vernarsky <soulish@gmail.com>
2015-12-23 20:43:36 -0500

    Add help function.

--- generateStandardDeviations.cpp
+++ generateStandardDeviations.cpp
@@ -14,6 +14,8 @@
 #include "src/ConstantTeamNeutralRatios.h"
 #include "src/ConstantSRSadditions.h"
 
+void printOptions();
+
 int main(int argc,char *argv[]){
     int c;
     int outYear = 0;
@@ -23,7 +25,7 @@ int main(int argc,char *argv[]){
     std::string outFileName = "";
 
     /*____________________________Parse Command Line___________________________*/
-    while ((c = getopt(argc, argv, "y:Y:vo:")) != -1) {
+    while ((c = getopt(argc, argv, "y:Y:vo:h")) != -1) {
         switch (c) {
             case 'y':
                 std::cout << "inYears: " << optarg << std::endl;
@@ -40,6 +42,9 @@ int main(int argc,char *argv[]){
                 outFileName.assign(optarg);
                 writeOutput = true;
                 break;
+            case 'h':
+                printOptions();
+                return 0;
             default:
                 // not an option
                 break;
@@ -50,10 +55,12 @@ int main(int argc,char *argv[]){
     if (years.empty()) {
         std::cout << "You must set the input years using the -y switch and a comma-separated list of years" <<
         std::endl;
+        printOptions();
         return 0;
     }
     if (outYear == 0) {
         std::cout << "You must set the output year using the -Y switch" << std::endl;
+        printOptions();
         return 0;
     }
 
@@ -156,3 +163,21 @@ int main(int argc,char *argv[]){
     return 0;
 }
 
+void printOptions(){//yYvo
+    std::cout << std::endl;
+    std::cout << "generateStandardDeviations Usage options:" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "\t-y (int,int,...) comma-separated list of input years (no default)[Required]" << std::endl;
+    std::cout << "\t-Y (int) output year (no default)[Required]" << std::endl;
+    std::cout << "\t-o (string) output file name, created in constants/ dir (no default)[Optional]" << std::endl;
+    std::cout << "\t-v verbose mode [Optional]" << std::endl;
+    std::cout << "\t-h print this message" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Ex: $CLION/generateStandardDeviations -y 2010,2011,2012,2013,2014 -Y 2015 -o waverage_standard_deviations.d" << std::endl;
+    std::cout << std::endl;
+    std::cout << "This program generates the standard deviations necessary for the game functions." << std::endl;
+    std::cout << "It takes as input 5 years worth of weighted averages and then outputs the standard" << std::endl;
+    std::cout << "deviations for a given year (set with -Y option) either to the terminal or" << std::endl;
+    std::cout << "to a file (set by the -o option)." << std::endl;
+    std::cout << std::endl;
+}
