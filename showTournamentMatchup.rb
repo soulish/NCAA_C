fd0f73f6b27ad067d4e42ae715be4138ef1051d2
Brian Vernarsky <soulish@gmail.com>
2015-12-25 01:39:11 -0500

    Add help function.  Add ability to choose SRS to select which weights and histograms files to use.

--- showTournamentMatchup.rb
+++ showTournamentMatchup.rb
@@ -4,17 +4,46 @@ gStyle.SetTitleFillColor(10)
 gStyle.SetTitleBorderSize(0)
 gStyle.SetPalette(1,0)
 
+def printOptions
+  puts ""
+  puts "showTournamentMatchup.rb Usage options"
+  puts ""
+  puts "\t-F (string) currently existing file name, or name of output ROOT file (no default)[Optional]"
+  puts "\t-t (string) teamnameA (no default)[Required if -F not used]"
+  puts "\t-T (string) teamnameB (no default)[Required if -F not used]"
+  puts "\t-H calculate and plot gameScore percentages"
+  puts "\t-s (int) (double) srs value to use for weights and histograms (default: \"fixed\")[Optional]"
+  puts "\t-h print this message"
+  puts ""
+  puts "Ex: ruby showTournamentMatchup.rb -t \"2015 north carolina\" -T \"2015 harvard\" -H -s 0.5"
+  puts
+  puts "This program is used to show the results of the showTournamentMatchup program, but"
+  puts "it can also be used to run that program first and then process the output, so as to make"
+  puts "this a one-step process.  If a ROOT file already exists, you can plot its graphs"
+  puts "using the -F switch to set the input file name. If a ROOT file does not already exist,"
+  puts "then you can create one by using the -t and -T options to set the names of the teams you want to plot."
+  puts "If you wish to save the ROOT file created, simply use the"
+  puts "-F option to set the name, it will be saved by default in the rootFiles/ directory, so"
+  puts "only the name is necessary.  If no name is set, then a temporary file is created which"
+  puts "is deleted at the end of the program."
+  puts ""
+end
+
 fileName = nil
 teamNameA = nil
 teamNameB = nil
-histogramsFileName = nil
+srsVal = "free"
+useHistograms = false
+
 #command line switcher
 ARGV.each_with_index do |entry, index|
   case entry
   when /^-F/
     fileName = ARGV[index+1]
+  when /^-s/
+    srsVal = ARGV[index+1]
   when /^-H/
-    histogramsFileName = ARGV[index+1]
+    useHistograms = true
   when /^-t/
     teamNameA = ARGV[index+1]
   when /^-T/
@@ -32,18 +61,18 @@ if (teamNameA.nil? && teamNameB.nil?)
   end
 else
   if fileName.nil?
-    if histogramsFileName.nil?
+    if !useHistograms
       `$CLION/showTournamentMatchup -t \"#{teamNameA}\" -T \"#{teamNameB}\" -o \"temp.root\"`
     else
-      `$CLION/showTournamentMatchup -t \"#{teamNameA}\" -T \"#{teamNameB}\" -o \"temp.root\" -H #{histogramsFileName}`
+      `$CLION/showTournamentMatchup -t \"#{teamNameA}\" -T \"#{teamNameB}\" -o \"temp.root\" -H -s #{srsVal}`
     end
     #sleep 3
     file = TFile.new("rootFiles/temp.root")
   else
-    if histogramsFileName.nil?
+    if !useHistograms
       `$CLION/showTournamentMatchup -t \"#{teamNameA}\" -T \"#{teamNameB}\" -o \"#{fileName}\"`
     else
-      `$CLION/showTournamentMatchup -t \"#{teamNameA}\" -T \"#{teamNameB}\" -o \"#{fileName}\" -H #{histogramsFileName}`
+      `$CLION/showTournamentMatchup -t \"#{teamNameA}\" -T \"#{teamNameB}\" -o \"#{fileName}\" -H -s #{srsVal}`
     end
     #sleep 3
     file = TFile.new("rootFiles/#{fileName}")
@@ -73,7 +102,7 @@ ss.each do |s|
   realLines[s] = TLine.new(lines[s].GetX1,lines[s].GetY1,lines[s].GetX2,lines[s].GetY2)
 end
 
-if !histogramsFileName.nil?
+if useHistograms
   avgsA_per_time["gameScore"] = gROOT.FindObject("avgA_per_time_gameScore")
   wavgsA_per_time["gameScore"] = gROOT.FindObject("wavgA_per_time_gameScore")
 
@@ -112,7 +141,7 @@ ss.each_with_index do |s,i|
   realLines[s].Draw("same")
 end
 
-if !histogramsFileName.nil?
+if useHistograms
   can.cd(11)
   wavgsA_per_time["gameScore"].SetMinimum(0)
   wavgsA_per_time["gameScore"].SetMaximum(1)
